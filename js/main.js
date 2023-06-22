2f74cffabc2103dd213aa3f66410573e21d27166
overlord <cesar.yeguez@gmail.com>
2023-06-21 21:55:53 -0400

    ajuste los estilos de mi navbar mode mobile

--- js/main.js
+++ js/main.js
@@ -1,6 +1,9 @@
 
 const nav = document.querySelector(".nav");
 const menu = document.getElementById("menu");
+const closeIcon = document.querySelector("#close-icon");
+
+console.log(closeIcon);
 
 const toggle = () => {
   
@@ -8,4 +11,12 @@ const toggle = () => {
   nav.classList.toggle("mostrar");
 }
 
+const prueba = () => {console.log('click')}
+
+function mensaje(){
+  console.log("click");
+
+}
 menu.addEventListener("click", toggle);
+closeIcon.addEventListener("click", toggle);
+
