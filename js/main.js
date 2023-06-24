85dd6dff78310fc4cc0e4a937ab17a51e9ee2a1a
cyeguez <cesar.yeguez@gmail.com>
2023-06-24 08:48:05 -0400

    se agrega información de la primera sección, se agrega la libreria typed.js para hacer un efecto en la presentación

--- js/main.js
+++ js/main.js
@@ -2,6 +2,28 @@
 const nav = document.querySelector(".nav");
 const menu = document.getElementById("menu");
 const closeIcon = document.querySelector("#close-icon");
+const typed = new Typed('.typed', {
+  strings: [
+    // '<i class="profesion">Técnologo en Sistemas Industriales</i>',
+    // '<i class="profesion">Desarrollador web Junior</i>',
+    // '<i class="profesion">Estudiante de programación</i>',
+
+
+    
+  ],
+  stringsElement: '#cadenas-texto', // ID del elemento que contiene cadenas de texto a mostrar.
+	typeSpeed: 50, // Velocidad en mlisegundos para poner una letra,
+	startDelay: 300, // Tiempo de retraso en iniciar la animacion. Aplica tambien cuando termina y vuelve a iniciar,
+	backSpeed: 30, // Velocidad en milisegundos para borrrar una letra,
+	smartBackspace: true, // Eliminar solamente las palabras que sean nuevas en una cadena de texto.
+	shuffle: false, // Alterar el orden en el que escribe las palabras.
+	backDelay: 1500, // Tiempo de espera despues de que termina de escribir una palabra.
+	loop: true, // Repetir el array de strings
+	loopCount: false, // Cantidad de veces a repetir el array.  false = infinite
+	showCursor: true, // Mostrar cursor palpitanto
+	cursorChar: '|', // Caracter para el cursor
+	contentType: 'html', // 'html' o 'null' para texto sin formato
+});
 
 console.log(closeIcon);
 
