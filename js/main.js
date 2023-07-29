157c186bf0b32ed17b73e82d607b32816f357a81
cyeguez <cesar.yeguez@gmail.com>
2023-07-29 11:30:41 -0400

    feat(Se agrega la seccion de el service, contact y footer en modo mobile)

--- js/main.js
+++ js/main.js
@@ -1,33 +1,20 @@
-const home = document.getElementById("home");
-const nav = document.querySelector(".nav");
-const menu = document.getElementById("menu");
-const closeIcon = document.querySelector("#close-icon");
-const navLink = document.querySelectorAll("nav a[href^='#']");
-let activePage = window.location.href;
+const menu = document.getElementById('menu');
+const iconClose = document.getElementById('icon-close');
+const nav = document.querySelector('.nav');
+const menuLink= document.querySelectorAll('.nav__container a[href^="#"]');
 
+console.log(menuLink);
 
-
-const toggleMenu = () => {
-  menu.classList.toggle("rotate");
-  nav.classList.toggle("hidden");
+const toogleMenu = ()=>{
+  nav.classList.toggle('active-menu');
   
-};
+}
 
+menuLink.forEach(link =>{
+  link.addEventListener('click', ()=>{
+    nav.classList.remove('active-menu')
+  })
+})
 
-  navLink.forEach((link) => {
-    link.addEventListener("click", function () {
-      nav.classList.remove('hidden');
-    });
-  });
-
-
-
-  
-
-
-
-
-menu.addEventListener("click", toggleMenu);
-
-closeIcon.addEventListener("click", toggleMenu);
-// nav.addEventListener("click", active);
+menu.addEventListener("click", toogleMenu);
+iconClose.addEventListener("click", toogleMenu)
